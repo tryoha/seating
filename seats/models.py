fc8a40df44cfec07765c9791e6ff71fb06d8369b
barmazon <e.tregubov@gmail.com>
2024-07-27 21:58:19 +0300

    verbose names

--- seats/models.py
+++ seats/models.py
@@ -2,16 +2,19 @@ from django.db import models
 
 
 class Place(models.Model):
-    place = models.CharField(max_length=60, unique=True)
-    max_seats = models.PositiveIntegerField()
+    place = models.CharField("Название ресторана", max_length=60, unique=True)
 
     def __str__(self):
         return self.place
 
+    class Meta:
+        verbose_name = "Ресторан"
+        verbose_name_plural = "Рестораны"
+
 
 class Seat(models.Model):
-    seat = models.PositiveIntegerField(unique=True)
-    available = models.BooleanField(default=True)
+    seat = models.PositiveIntegerField("Место", unique=True)
+    available = models.BooleanField("Доступность", default=True)
     place = models.ForeignKey(Place, on_delete=models.CASCADE, related_name="seats")
 
     def __str__(self):
@@ -19,3 +22,5 @@ class Seat(models.Model):
 
     class Meta:
         ordering = ["seat"]
+        verbose_name = "Место"
+        verbose_name_plural = "Места"
