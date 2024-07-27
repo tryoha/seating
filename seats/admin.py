dc8b635ddb58ddda7221a84c04807517a765b456
barmazon <e.tregubov@gmail.com>
2024-07-27 21:58:34 +0300

    filters

--- seats/admin.py
+++ seats/admin.py
@@ -1,5 +1,3 @@
-from typing import Literal
-
 from django.contrib import admin
 
 from .models import Place, Seat
@@ -7,10 +5,15 @@ from .models import Place, Seat
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    list_display = ("place", "max_seats")
+    list_display = ("place",)
 
 
 @admin.register(Seat)
 class SeatAdmin(admin.ModelAdmin):
     list_display = ("seat", "place", "available")
     list_editable = ("available",)
+    list_filter = ("available", "place")
+    search_fields = ("seat",)
+
+    def place(self, obj):
+        return obj.place
