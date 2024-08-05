3ebcf788ac4e5efc3f3e63987ffa004c670b1c60
barmazon <e.tregubov@gmail.com>
2024-08-05 20:35:52 +0300

    static in debug

--- project/urls.py
+++ project/urls.py
@@ -1,4 +1,6 @@
 from debug_toolbar.toolbar import debug_toolbar_urls
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
@@ -6,3 +8,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("", include("seats.urls")),
 ] + debug_toolbar_urls()
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
