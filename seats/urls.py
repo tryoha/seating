e715ed2646857354143b101107386dff5f275bb9
barmazon <e.tregubov@gmail.com>
2024-08-05 20:36:29 +0300

    test views for places

--- seats/urls.py
+++ seats/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
 
-from .views import index
+from .views import ballroom_index, grilled_index, index, mangup_index, tavrika_index
 
 app_name = "seats"
 
 urlpatterns = [
-    path("", index, name="index"),
+    path("", ballroom_index, name="main"),
+    path("ballroom/", ballroom_index, name="ballroom"),
+    path("grilled/", grilled_index, name="grilled"),
+    path("mangup/", mangup_index, name="mangup"),
+    path("tavrika/", tavrika_index, name="tavrika"),
 ]
--- seats/views.py
+++ seats/views.py
@@ -1,13 +1,48 @@
 from django.shortcuts import render
 
-from .models import Seat
+from .models import Place, Seat
 
 
 def index(request):
-    seats = Seat.objects.all()
+    places = Place.objects.all()
+    return render(request, "seats/index.html", {"places": places})
+
+
+def ballroom_index(request):
+    seats = Seat.objects.filter(place__name="ballroom")
     context = {}
 
     for seat in seats:
         context[seat.seat] = {"place": seat.place, "available": seat.available}
 
-    return render(request, "seats/index.html", context)
+    return render(request, "seats/ballroom.html", context)
+
+
+def grilled_index(request):
+    seats = Seat.objects.filter(place__name="grilled")
+    context = {}
+
+    for seat in seats:
+        context[seat.seat] = {"place": seat.place, "available": seat.available}
+
+    return render(request, "seats/grilled.html", context)
+
+
+def mangup_index(request):
+    seats = Seat.objects.filter(place__name="mangup")
+    context = {}
+
+    for seat in seats:
+        context[seat.seat] = {"place": seat.place, "available": seat.available}
+
+    return render(request, "seats/mangup.html", context)
+
+
+def tavrika_index(request):
+    seats = Seat.objects.filter(place__name="tavrika")
+    context = {}
+
+    for seat in seats:
+        context[seat.seat] = {"place": seat.place, "available": seat.available}
+
+    return render(request, "seats/tavrika.html", context)
