1d88e82064067359b6f88ae6bca0efc503403bc5
barmazon <e.tregubov@gmail.com>
2024-08-07 22:58:45 +0300

    added mob main menu dynamics

--- seats/static/seats/js/script.js
+++ seats/static/seats/js/script.js
@@ -4,6 +4,8 @@ let cntctsMenu = document.getElementById("popup-contacts");
 let hotelsMenu = document.getElementById("popup-hotels");
 let hotelsOverlay = document.getElementById("popup-overlay-hotels");
 let cntctsOverlay = document.getElementById("popup-overlay");
+let hamburger = document.getElementById("mobile-menu");
+let mainMenu = document.getElementById("main-menu");
 
 
 function displayContactsOverlay() {
@@ -27,7 +29,18 @@ function hideHotelsOverlay() {
   hotelsOverlay.classList.add('closed');
 }
 
+function openMobileMenu() {
+    mainMenu.classList.remove('mobile-menu-hidden');
+}
+
+function closeMobileMenu() {
+    mainMenu.classList.add('mobile-menu-hidden');
+}
+
 cntctsMenu.addEventListener("click", displayContactsOverlay);
 cntctsOverlay.addEventListener("click", hideContactsOverlay);
 hotelsMenu.addEventListener("click", openHotelOverlay);
 hotelsOverlay.addEventListener("click", hideHotelsOverlay);
+hamburger.addEventListener("click", openMobileMenu);
+mainMenu.addEventListener("click", closeMobileMenu);
+
--- static/seats/js/script.js
+++ static/seats/js/script.js
@@ -1,1 +1,46 @@
-console.log("js тут")
+"use strict"
+
+let cntctsMenu = document.getElementById("popup-contacts");
+let hotelsMenu = document.getElementById("popup-hotels");
+let hotelsOverlay = document.getElementById("popup-overlay-hotels");
+let cntctsOverlay = document.getElementById("popup-overlay");
+let hamburger = document.getElementById("mobile-menu");
+let mainMenu = document.getElementById("main-menu");
+
+
+function displayContactsOverlay() {
+    cntctsOverlay.classList.remove("closed");
+    cntctsOverlay.classList.add("opened");
+}
+
+
+function hideContactsOverlay() {
+  contactsOverlay.classList.remove("opened");
+  contactsOverlay.classList.add("closed");
+}
+
+function openHotelOverlay() {
+    hotelsOverlay.classList.remove('closed');
+    hotelsOverlay.classList.add('opened');
+}
+
+function hideHotelsOverlay() {
+  hotelsOverlay.classList.remove('opened');
+  hotelsOverlay.classList.add('closed');
+}
+
+function openMobileMenu() {
+    mainMenu.classList.remove('mobile-menu-hidden');
+}
+
+function closeMobileMenu() {
+    mainMenu.classList.add('mobile-menu-hidden');
+}
+
+cntctsMenu.addEventListener("click", displayContactsOverlay);
+cntctsOverlay.addEventListener("click", hideContactsOverlay);
+hotelsMenu.addEventListener("click", openHotelOverlay);
+hotelsOverlay.addEventListener("click", hideHotelsOverlay);
+hamburger.addEventListener("click", openMobileMenu);
+mainMenu.addEventListener("click", closeMobileMenu);
+
